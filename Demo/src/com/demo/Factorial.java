6e7bbba8ffb9f9be641046400fdf7ccff423a924
chandana <maddukurichandana@gmail.com>
2022-03-17 12:46:06 +0530

    initial commit

---
+++ Demo/src/com/demo/Factorial.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner scan =new Scanner(System.in);
+		System.out.println("enter a number");
+		int n=scan.nextInt();
+	    int fact=1;
+		for(int i=1;i<=n;i++) {
+		 fact=fact*i;
+		}
+ System.out.println("Factorial of number is: "+fact);
+	}
+
+}
